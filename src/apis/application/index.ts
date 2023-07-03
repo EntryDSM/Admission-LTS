c3e26b757e11960460fc266770fc33c411d35a94
kimjh11130 <kimjh11130@naver.com>
2023-07-04 01:32:45 +0900

    fix: api router 추가

--- src/apis/application/index.ts
+++ src/apis/application/index.ts
@@ -30,7 +30,7 @@ export const EditUserInfo = () => {
 /** 유저 이름, 전화번호 조회 */
 export const GetUserInfos = () => {
   const response = async () => {
-    const { data } = await instance.get<{ name: string; telephone_number: string }>('application/users/info');
+    const { data } = await instance.get<{ name: string; telephone_number: string }>(`${router}/users/info`);
     return data;
   };
   return useQuery(['userInfos'], response);
