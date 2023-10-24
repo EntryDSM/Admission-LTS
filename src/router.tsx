bdb4f29bf3287378f11b5ce9858f4a791f4bb2fc
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-24 16:38:55 +0900

    fix: 빌드 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ApplicationPage from './pages/Application';
 import NotFound from './pages/NotFound';
